6f983c5aac7485fee1de299c2bb07e768586b5d7
biomurph <biomurph@gmail.com>
2018-02-21 09:38:32 -0500

    Better EMA filter, beat finder

--- SFE_HeartRate_ble_sleep_OLED/SFE_HeartRate_ble_sleep_OLED.ino
+++ SFE_HeartRate_ble_sleep_OLED/SFE_HeartRate_ble_sleep_OLED.ino
@@ -45,7 +45,7 @@
 #include <Lazarus.h>
 Lazarus Lazarus;
 
-#include <ota_bootloader.h> 
+#include <ota_bootloader.h>
 #include <SimbleeBLE.h>
 
 
@@ -77,16 +77,23 @@ int sleepTime = 600; //600 is production
 
 float volts = 8;
 
-//const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
-//byte rates[RATE_SIZE]; //Array of heart rates
-//byte rateSpot = 0;
+float EMA_a = 0.4;    //initialization of EMA alpha
+long sumEMA_S = 0;        //initialization of sum EMA S
+int irEMA_S = 0;        //initialization of IR EMA S
+int redEMA_S = 0;        //initialization of RED EMA S
+int greenEMA_S = 0;        //initialization of GREEN EMA S
+int sumHighpass = 0;
+int irHighpass = 0;
+int redHighpass = 0;
+int greenHighpass = 0;
+int sumSample = 0;
+int irSample = 0;
+int redSample = 0;
+int greenSample = 0;
 long lastBeatTime = 0; //Time at which the last beat occurred
 bool firstBeat = true;
 float beatsPerMinute;
-//int beatAvg;
-//uint8_t lastBeatAvg;
-//uint8_t aveBeatsAve[255];
-//uint8_t aveCounter = 0;
+
 int beat;
 uint8_t lastBeat;
 uint8_t arrayBeats[256];
@@ -177,7 +184,7 @@ void setup()
   pinMode(BMI_INT1, INPUT); // set BMI interrupt pin
   Simblee_pinWake(BMI_INT1, LOW); // use this to wake the MCU if its sleeping
 
-  
+
 #ifdef DEBUG
   Serial.begin(9600);
   Serial.println("Initializing...");
@@ -193,9 +200,20 @@ setTime(DEFAULT_TIME);
     while (1);
   }
 
-  particleSensor.setup(); //Configure sensor with default settings
-  //particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-  //particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+  //Setup to sense a nice looking saw tooth on the plotter
+  //  *Joel's notes  -> NEEDS MOVE TO DEFINES
+  byte ledBrightness = 0x1F; //Options: 0=Off to 255=50mA *not a bad value
+  byte sampleAverage = 8; //Options: 1, 2, 4, 8, 16, 32 *have not messed with this
+  byte ledMode = 3; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green *not a bad value?
+  byte sampleRate = 200; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200  *higher = noisier but better resolution
+  int pulseWidth = 215; //Options: 69, 118, 215, 411  *shorteded from 411
+  int adcRange = 2048; //Options: 2048, 4096, 8192, 16384 *smaller range = larger sample number
+
+  firstBeat = true;
+  beat = lastBeat = beatCounter = 0;
+
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
 
   //Blink the startup pattern
   digitalWrite(RED, LOW);
@@ -280,14 +298,17 @@ void loop()
       samples[currentSample].epoch = utc;  // Send utc time to the phone. Phone will manage timezone, etc.
       samples[currentSample].steps = BMI160.getStepCount();
       memset(arrayBeats, 0, sizeof(arrayBeats));
-      firstBeat = true; 
+      firstBeat = true;
       beat = lastBeat = beatCounter = 0;
 #ifdef DEBUG
       Serial.println("Starting HR capture");
 #endif
       printOLED("Measuring Heart Rate",true);
-//      aveCounter = 0;                       // used with original beat counter code
-//      beatAvg = lastBeatAvg = rateSpot = 0; // used with original beat counter code
+      delay(1500);
+      sumEMA_S = particleSensor.getIR() + particleSensor.getRed(); // + particleSensor.getGreen();     //set filter sumEMA S for t=1
+      // irEMA_S = particleSensor.getIR();           //set filter irEMA S for t=1
+      // redEMA_S = particleSensor.getRed();         //set filter redEMA S for t=1
+      // greenEMA_S = particleSensor.getGreen();     //set filter greenEMA S for t=1
       startTime = millis();
       while (captureHR(startTime)) {
         ;
@@ -308,7 +329,7 @@ void loop()
       //                samples[currentSample].aux1 = analogRead(PIN_2);
       //                samples[currentSample].aux2 = analogRead(PIN_3);
       //                samples[currentSample].aux3 = analogRead(PIN_4);
-      
+
       bpm = ""; // clear the bpm string
       bpm += String(samples[currentSample].hr);
       bpm += " BPM";
@@ -334,7 +355,7 @@ void loop()
       Serial.print("Samples Captured: ");
       Serial.println(currentSample);
 #endif
-  
+
       awakeTime = millis() - lastTime;
       sleepTimeNow = sleepTime - (interval / 1000);
       sleepNow(sleepTimeNow);
@@ -437,5 +458,3 @@ uint8_t getBatteryVoltage() {
 
   return volts / BATT_VOLT_CONST;
 }
-
-
--- SFE_HeartRate_ble_sleep_OLED/heartRate_stuff.ino
+++ SFE_HeartRate_ble_sleep_OLED/heartRate_stuff.ino
@@ -1,6 +1,103 @@
 
 
+long lastBeatTime = 0; //Time at which the last beat occurred
+float beatsPerMinute;
 
+//long interval = 30000; //30000 this is how long we capture hr data
+
+bool captureHR(uint32_t startTime) {
+  if (millis() - startTime > interval) {
+#ifdef DEBUG
+    Serial.println("HR capture done");
+#endif
+    return(false);
+  }
+
+  redSample = particleSensor.getRed();
+  irSample = particleSensor.getIR();
+  sumSample = redSample + irSample;
+
+  sumSample = irSample + redSample; // + greenSample; * seems to be the best
+  sumEMA_S = (EMA_a*sumSample) + ((1-EMA_a)*sumEMA_S);  //run the EMA
+  sumHighpass = sumSample - sumEMA_S;                           //calculate the high-pass signal
+
+  while (SimbleeBLE.radioActive) {
+    ;
+  }
+
+  if (checkForPulse(sumHighpass) == true)
+  {
+    analogWrite(RED, 200);
+    //We sensed a beat!
+    if(firstBeat){
+      firstBeat = false;
+      lastBeatTime = millis();
+      return(true);
+    }
+    long delta = millis() - lastBeatTime;
+    lastBeatTime = millis();
+
+    beatsPerMinute = 60 / (delta / 1000.0); // get instantaneous BPM
+
+    if (beatsPerMinute < 200 && beatsPerMinute > 50)
+    {
+        beat = beatsPerMinute;
+        arrayBeats[beatCounter] = (uint8_t)beat;
+        beatCounter++;
+        if(beatCounter > 255){ beatCounter = 0; }
+    #ifdef DEBUG
+        Serial.print("instant BPM: ");
+        Serial.println(beat);
+    #endif
+    }
+  }
+  digitalWrite(RED, HIGH);
+
+
+}
+
+
+
+
+bool checkForPulse(int sample){
+
+boolean beatDetected = false;
+  if (sample < T){
+    if(!rising){
+      T = sample;
+    }else{
+      rising = true;
+      edge = true;
+    }
+  }
+  if (sample > P){
+    if(rising){
+      P = sample;
+    }else{
+      rising = false;
+      edge = true;
+    }
+  }
+
+  if(edge){
+    edge = false;
+    amp = P-T;
+    if(rising){
+      T = P;
+    }else{
+      T = P;
+    }
+    if(amp>50 && amp<6000){
+      beatDetected = true;
+    }
+  }
+  return beatDetected;
+  }
+
+
+
+
+/*
 bool captureHR(uint32_t startTime) {
   if (millis() - startTime > interval) {
 #ifdef DEBUG
@@ -54,7 +151,7 @@ bool captureHR(uint32_t startTime) {
     // if(bConnected) {
     //         SimbleeBLE.sendInt(beatAvg);
     // }
-    
+
     lastBeat = beat;
   }
   return(true);
@@ -64,7 +161,7 @@ bool captureHR(uint32_t startTime) {
 
 /*
  *        OLD AVERAGING CODE
- * 
+ *
  *    rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
       rateSpot %= RATE_SIZE; //Wrap variable
       if(rateSpot == RATE_SIZE-1){
@@ -73,5 +170,5 @@ bool captureHR(uint32_t startTime) {
         for (byte x = 0; x < RATE_SIZE; x++)
           beatAvg += rates[x];
         beatAvg /= RATE_SIZE;
- * 
+ *
  */
